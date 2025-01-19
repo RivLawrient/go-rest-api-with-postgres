d91f27363617eb09935bc49c1d5dc0f8d722ca9e
lawrient <sandincl.d@gmail.com>
2025-01-19 16:26:19 +0800

    example simple clean code api

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+// Handler untuk root
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome to the root!")
+	fmt.Println("Root handler called")
+}
+
+// Handler untuk /hello
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+	fmt.Println("Hello handler called")
+}
+
+// Handler untuk /about
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "About page")
+	fmt.Println("About handler called")
+}
+
+// membungus semua endpoint
+func Router() http.Handler {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", rootHandler)
+	mux.HandleFunc("/hello", helloHandler)
+	mux.HandleFunc("/about", aboutHandler)
+
+	return mux
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	// create config server like port and ip
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: Router(),
+	}
+
+	fmt.Printf("server running at http://%s \n\n", server.Addr)
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 }
