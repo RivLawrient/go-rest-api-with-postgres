63d2b704db6b15dd0cad7125d2e1fe5e1430190f
lawrient <sandincl.d@gmail.com>
2025-01-24 21:04:50 +0800

    router method

--- internal/app/wallet/controller_wallet.go
+++ internal/app/wallet/controller_wallet.go
@@ -2,7 +2,6 @@ package wallet
 
 import (
 	"encoding/json"
-	"go-rest-api-with-postgres/internal/helper"
 	"go-rest-api-with-postgres/internal/model"
 	"net/http"
 	"strings"
@@ -17,11 +16,18 @@ func NewWalletController(walletUsecase *WalletUsecase) *WalletController {
 		WalletUsecase: walletUsecase,
 	}
 }
+func (w *WalletController) Handle(res http.ResponseWriter, req *http.Request) {
+	// if helper.ValidateMethod(res, req, "GET") {
+	// 	return
+	// }
+	res.WriteHeader(http.StatusOK)
+	res.Write([]byte("berhasil"))
+}
 
 func (w *WalletController) HandleNewWallet(res http.ResponseWriter, req *http.Request) {
-	if helper.ValidateMethod(res, req, "POST") {
-		return
-	}
+	// if helper.ValidateMethod(res, req, "POST") {
+	// 	return
+	// }
 
 	res.Header().Set("Content-Type", "application/json")
 
--- internal/router/router.go
+++ internal/router/router.go
@@ -1,9 +1,40 @@
 package router
 
 import (
+	"encoding/json"
+	"fmt"
 	"go-rest-api-with-postgres/internal/app/wallet"
+	"go-rest-api-with-postgres/internal/model"
 	"net/http"
 )
+
+// membungkus fungsi Handle di berbagai method
+type MethodHandlers struct {
+	get    func(http.ResponseWriter, *http.Request)
+	post   func(http.ResponseWriter, *http.Request)
+	put    func(http.ResponseWriter, *http.Request)
+	delete func(http.ResponseWriter, *http.Request)
+}
+
+// menggabungkan fungsi Handle jika berada di endpoint yang sama namun method yang berbeda
+func (mh *MethodHandlers) Handle(w http.ResponseWriter, r *http.Request) {
+	handler := map[string]func(http.ResponseWriter, *http.Request){
+		http.MethodGet:    mh.get,
+		http.MethodPost:   mh.post,
+		http.MethodPut:    mh.put,
+		http.MethodDelete: mh.delete,
+	}[r.Method]
+
+	if handler != nil {
+		handler(w, r)
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		json.NewEncoder(w).Encode(model.WebResponse[string]{
+			Errors: fmt.Sprintf("method %s is not found", r.Method),
+		})
+	}
+}
 
 type RouterConfig struct {
 	Routing          *http.ServeMux
@@ -14,5 +45,10 @@ type RouterConfig struct {
 // menghindari penggunaan root endpoint ("/") agar menghasilkan 404 page not found.
 func (c *RouterConfig) Route() {
 
-	c.Routing.HandleFunc("/wallet", c.WalletController.HandleNewWallet)
+	walletHandle := &MethodHandlers{
+		post: c.WalletController.HandleNewWallet,
+		get:  c.WalletController.Handle,
+	}
+	c.Routing.HandleFunc("/wallet", walletHandle.Handle)
+
 }
