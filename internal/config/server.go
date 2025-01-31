a38f1b0ed19c724a9e457bf2a6236464971ce6da
lawrient <sandincl.d@gmail.com>
2025-01-31 18:03:10 +0800

    cors

--- internal/config/server.go
+++ internal/config/server.go
@@ -11,7 +11,7 @@ func EnableCORS(next http.Handler) http.Handler {
 		// Atur header CORS
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		// w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		// Lanjutkan ke handler berikutnya
 		next.ServeHTTP(w, r)
@@ -19,7 +19,6 @@ func EnableCORS(next http.Handler) http.Handler {
 }
 
 func NewServer(viper *viper.Viper, app *http.ServeMux) *http.Server {
-
 	cors := EnableCORS(app)
 	server := http.Server{
 		Addr:    viper.GetString("SERVER_ADDR"),
