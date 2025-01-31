0f50dcec2baacd0ae6f66162e48bda0cd6eb3d5e
lawrient <sandincl.d@gmail.com>
2025-01-31 16:59:55 +0800

    cors

--- internal/config/server.go
+++ internal/config/server.go
@@ -6,10 +6,24 @@ import (
 	"github.com/spf13/viper"
 )
 
+func EnableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Atur header CORS
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		// Lanjutkan ke handler berikutnya
+		next.ServeHTTP(w, r)
+	})
+}
+
 func NewServer(viper *viper.Viper, app *http.ServeMux) *http.Server {
+
+	cors := EnableCORS(app)
 	server := http.Server{
 		Addr:    viper.GetString("SERVER_ADDR"),
-		Handler: app,
+		Handler: cors,
 	}
 
 	return &server
